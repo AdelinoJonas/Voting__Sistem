8b4f5b0d5ce7160907355c023f92ca134366d42a
AdelinoJonas <84252481+AdelinoJonas@users.noreply.github.com>
2022-10-16 15:04:16 -0300

    get all polls working

--- API_Voting_Sistem/src/controllers/pollController.js
+++ API_Voting_Sistem/src/controllers/pollController.js
@@ -1,27 +1,19 @@
 const pollService = require('../services/pollServices');
 
 module.exports = {
-    getAll: async (req, res) => {
-        let json = {
-            error: '',
-            result: []
-        };
+  getAll: async (req, res) => {
+    let json = {
+      error: '',
+      result: []
+    };
 
-        let polls = await pollService.getAll();
+    let polls = await pollService.getAll();
 
-        for (let i in polls) {
-            json.result.push({
-                id: polls[i].id,
-                registerDate: polls[i].registerDate,
-                title: polls[i].title,
-                questionDescription: polls[i].questionDescription,
-                startDate: polls[i].startDate,
-                endDate: polls[i].endDate,
-                idOption: options[i].idOption,
-                optionDescription: options[i].optionDescription,
-                totalVotes: options[i].totalVotes
-            })
-        }
-        res.json(json);
+    if (polls) {
+      json.result.push(
+        polls
+      )
     }
+    res.json(json);
+  }
 }
--- API_Voting_Sistem/src/services/pollServices.js
+++ API_Voting_Sistem/src/services/pollServices.js
@@ -3,12 +3,15 @@ const db = require('../db');
 module.exports = {
     getAll: () => {
         return new Promise((resolve, reject) => {
-            db.query('SELECT * FROM poll_questions', (error, results) => {
+            const sql = 'SELECT id, registerDate, startDate, endDate, title, questionDescription, option1, option2, option3, votesOption1, votesOption2,votesOption3 FROM poll_options as o left outer JOIN options_votes as v ON o.idOption = v.idVotes left JOIN poll_questions as q ON o.idOption = q.id';
+
+            db.query(sql, (error, results) => {
                 if (error) {
                     reject(error);
                     return;
                 }
                 resolve(results);
+                console.log(results);
             });
         });
     }
