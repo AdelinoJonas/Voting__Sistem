e12244947a040dbd5967a6842d9c61b399bf6476
Jonas Adelino <84252481+AdelinoJonas@users.noreply.github.com>
2022-10-17 17:47:25 -0300

    home and card completed

---
+++ FrontEnd_Voting_Sistem/src/components/Menu/index.js
@@ -0,0 +1,16 @@
+import './style.css';
+
+function Menu(){
+    return (
+        <div>
+            <ul className='menu'>
+                <li>Cadastrar</li>
+                <li>Editar</li>
+                <li>Excluir</li>
+                <li>pesquisar</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Menu;
